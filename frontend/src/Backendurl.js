046ca347524644a3b6222fec79566d91cf9ea2e7
Mo <Mhd.shn.web.edu@gmail.com>
2025-02-27 13:35:56 +0530

    done

--- frontend/src/Backendurl.js
+++ frontend/src/Backendurl.js
@@ -1,9 +1,9 @@
 // export const backEndURL = "https://54.164.109.11:3001";
 // export const imageURL = "https://54.164.109.11:3001";
 
-export const imageURL = "http://localhost:3001";
-export const backEndURL = "http://localhost:3001";
+// export const imageURL = "http://localhost:3001";
+// export const backEndURL = "http://localhost:3001";
 
-// export const backEndURL = "https://rtechsl.lk";
-// export const imageURL = "https://rtechsl.lk";
+export const backEndURL = "https://rtechsl.lk";
+export const imageURL = "https://rtechsl.lk";
 
